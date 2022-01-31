d57a28b5158e53c4b40d2eb7c325a604e408e81b
Gustavo Benitez <benitezlgustavo@gmail.com>
2022-01-30 18:38:59 -0600

    Revert "Buffer array"
    This reverts commit 2f8b47c8cc1dfe95af07c5f09c0831e98cacb1d8.

--- GUI/src/Buffer.java
+++ GUI/src/Buffer.java
@@ -6,39 +6,38 @@ import java.util.logging.Logger;
 
 public class Buffer {
     
-    private Operation[] buffer = new Operation[10]; 
+    private Operation buffer = new Operation(); 
     
     Buffer() {
-        buffer[0] = new Operation();
-        this.buffer[0].flag = -1;
+        this.buffer.flag = -1;
     }
     
     synchronized Operation consume() {
         Operation product = new Operation();
         
-        if(this.buffer[0].flag == -1) {
+        if(this.buffer.flag == -1) {
             try {
                 wait();
             } catch (InterruptedException ex) {
                 Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        product = this.buffer[0];
-        this.buffer[0].flag = -1;
+        product = this.buffer;
+        this.buffer.flag = -1;
         notify();
         
         return product;
     }
     
     synchronized void produce(Operation product) {
-        if(this.buffer[0].flag != -1) {
+        if(this.buffer.flag != -1) {
             try {
                 wait();
             } catch (InterruptedException ex) {
                 Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        this.buffer[0] = product;
+        this.buffer = product;
         
         notify();
     }
