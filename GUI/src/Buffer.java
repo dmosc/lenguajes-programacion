9a7b3b58f65c8886788c4bd0c515515f6a0a4164
oscardavidrm <oscardavidrodriguezm@gmail.com>
2022-02-02 07:09:23 -0600

    Remove commented code.

--- GUI/src/Buffer.java
+++ GUI/src/Buffer.java
@@ -3,15 +3,13 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JTable;
-import javax.swing.JProgressBar;
 import javax.swing.table.DefaultTableModel;
 
 public class Buffer {
 
     private final Lock _mutex = new ReentrantLock(true);
-    Operation buffer[]; // circular queue
-    int left, right, next;
+    Operation buffer[];
+    int next;
     static boolean stop = false;
     private DefaultTableModel model;
     private javax.swing.JProgressBar jProgressBar;
@@ -19,20 +17,13 @@ public class Buffer {
 
     Buffer(int size, javax.swing.JTable _jTable, javax.swing.JProgressBar _jProgressBar) {
         buffer = new Operation[size];
-        left = 0;
-        right = 0;
+        next = 0;
         model = (DefaultTableModel) _jTable.getModel();
         jProgressBar = _jProgressBar;
         min_bar = jProgressBar.getMinimum();
         max_bar = jProgressBar.getMaximum();
-        next = 0;
     }
 
-    /*
-     * boolean isEmpty() {
-     * return left == right;
-     * }
-     */
     boolean isEmpty() {
         return next == 0;
     }
@@ -41,12 +32,6 @@ public class Buffer {
         return next + 1 >= buffer.length;
     }
 
-    /*
-     * boolean isFull() {
-     * return Math.abs(right - left) == 1 || (left == 0 && right == buffer.length -
-     * 1) || (left == buffer.length - 1 && right == 0);
-     * }
-     */
     synchronized Operation consume() {
         Operation product = null;
 
@@ -61,8 +46,6 @@ public class Buffer {
         _mutex.lock();
         try {
             product = buffer[--next];
-            // product = buffer[left];
-            // left = (left + 1) % buffer.length;
             model.removeRow(0);
             jProgressBar.setValue((int) (100 * model.getRowCount() / (buffer.length)));
         } finally {
@@ -84,19 +67,7 @@ public class Buffer {
         _mutex.lock();
         try {
             buffer[next++] = product;
-            /*
-             * System.out.println("------------------------------");
-             * for (int i = 0; i < buffer.length; i++) {
-             * if (buffer[i] != null) {
-             * System.out.println(buffer[i].formatted());
-             * }
-             * }
-             */
-            // buffer[right] = product;
-            // if (!isFull()) {
-            // right = (right + 1) % buffer.length;
-            // }
-            String[] values = { id, product.formatted() };
+            String[] values = {id, product.formatted()};
             model.addRow(values);
             jProgressBar.setValue((int) (100 * model.getRowCount() / buffer.length));
         } finally {
--- GUI/src/Consumer.java
+++ GUI/src/Consumer.java
@@ -1,7 +1,6 @@
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Consumer extends Thread {
